925ec167bed3f61699faa2a7ede8ad7eefa78dde
Reahana <reahana233@gmail.com>
2023-07-10 12:50:25 +0600

    stat

--- reactjs-core-concepts2/src/App.js
+++ reactjs-core-concepts2/src/App.js
@@ -12,9 +12,9 @@ function App() {
   // ]
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
 
-
+      <ExternalUsers></ExternalUsers>
 
 
       {/* {
@@ -28,6 +28,31 @@ function App() {
   );
 }
 
+function ExternalUsers (){
+  const [users, setUsers] = useState([]);
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, []);
+  return(
+    <div>
+      <h1>External User</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  );
+}
+function User(props){
+  return (
+    <div style={{border: '2px solid red', margin:'20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
 
 function Counter(){
   const [count, setCount] = useState(55);
