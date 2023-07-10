cbc59d31984f2fcfcf4778cb9793d4efec29cb66
Reahana <reahana233@gmail.com>
2023-07-10 11:38:00 +0600

    product function

--- reactjs-core-concepts2/src/App.js
+++ reactjs-core-concepts2/src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   
+   <Product></Product>
     </div>
   );
+}
+function Product(props){
+  return (
+    <div className="product">
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  )
 }
 
 export default App;
