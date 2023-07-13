05d17c850698f7adb5402bc659f1081ae26b071a
Reahana <reahana233@gmail.com>
2023-07-13 11:05:56 +0600

    watch

--- understand-react-core-concept/src/components/Watch/Watch.jsx
+++ understand-react-core-concept/src/components/Watch/Watch.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Watch = () => {
+    const abc = useState();
+    console.log(abc);
     return (
         <div>
             <h2>This is my smart Watch</h2>
+            <p>Steps =</p>
         </div>
     );
 };
